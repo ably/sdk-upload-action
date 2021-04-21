7ad71920e6f07d6ed8a6f3a93642d296c2ae903e
Owen Pearson <owen.pearson@ably.com>
2021-04-21 18:39:31 +0100

    initial implementation of s3 upload

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,94 @@
 import core from '@actions/core';
-import { S3Client } from "@aws-sdk/client-s3";
+import { context } from '@actions/github';
+import { S3Client, PutObjectCommand, PutObjectCommandInput } from "@aws-sdk/client-s3";
+import path from "path";
+import fs from "fs";
+import { lookup } from 'mime-types';
+
+const githubEventPath = process.env.GITHUB_EVENT_PATH;
+const githubRef = process.env.GITHUB_REF;
+
+if (typeof githubEventPath !== 'string') {
+    core.setFailed('GITHUB_EVENT_PATH environment variable not set');
+    process.exit(1);
+}
+
+if (typeof githubRef !== 'string') {
+    core.setFailed('GITHUB_REF environment variable not set');
+    process.exit(1);
+}
+
+const evt = JSON.parse(fs.readFileSync(githubEventPath, 'utf8'));
+
+// githubRef is in the form 'refs/heads/branch_name' so we have to slice away the 'refs/heads/' bit
+const branchName = githubRef.split('/').slice(2).join('/');
+
+const bucketName = 'sdk.ably.com';
+const sourcePath = path.resolve(core.getInput('sourcePath'));
+const destinationPath = path.resolve(core.getInput('destinationPath') ?? '');
+
+let keyPrefix = `builds/${context.repo}/`;
+if (context.eventName === 'pull_request') {
+    keyPrefix += `pull/${evt.pull_request.number}/`;
+} else if (context.eventName === 'push' && branchName === 'main') {
+    keyPrefix += 'main/';
+} else {
+    core.setFailed("Error: this action can only be ran on a pull_request or a push to the 'main' branch");
+    process.exit(1);
+}
+keyPrefix += destinationPath;
 
 const s3ClientConfig = {
-  // RegionInputConfig
-  region: 'eu-west-2',
+    // RegionInputConfig
+    region: 'eu-west-2',
 
-  // AwsAuthInputConfig
-  credentials: {
-    accessKeyId: core.getInput('s3AccessKeyId'),
-    secretAccessKey: core.getInput('s3AccessKey')
-  },
+    // AwsAuthInputConfig
+    credentials: {
+        accessKeyId: core.getInput('s3AccessKeyId'),
+        secretAccessKey: core.getInput('s3AccessKey')
+    },
 };
 
 const s3Client = new S3Client(s3ClientConfig);
 
-// TODO recurstively upload folder structure to S3
+const listFiles = (dir: string) => {
+    let files: string[] = [];
+    const filesInDir = fs.readdirSync(dir);
+    filesInDir.forEach(file => {
+        const name = `${dir}/${file}`;
+        if (fs.statSync(name).isDirectory()) {
+            files.push(...listFiles(name));
+        } else {
+            files.push(name);
+        }
+    });
 
-// https://gist.github.com/jlouros/9abc14239b0d9d8947a3345b99c4ebcb#gistcomment-2751992
+    return files;
+}
 
-// call send on s3Client with instance of:
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/classes/putobjectcommand.html
+const allFiles = listFiles(sourcePath);
 
-// Other references:
-// - https://www.npmjs.com/package/@actions/core
-// - https://github.com/actions/javascript-action
-// - https://github.com/actions/upload-artifact
+const upload = async (params: PutObjectCommandInput) => {
+    const command = new PutObjectCommand(params);
+    await s3Client.send(command);
+    core.info(`uploaded: ${params.Key}`);
+}
 
-// This repository inspired me as to how simple this might be able to be.
-// However, on closer inspection alongside the aws-sdk API reference, I'm not convinced
-// that they are doing it in the best or up-to-date manner.
-// - https://github.com/shallwefootball/upload-s3-action
+const run = async () => {
+    return Promise.all(allFiles.map(file => {
+        const body = fs.readFileSync(file);
+        const key = keyPrefix + path.relative(sourcePath, file);
+        return upload({
+            Key: key,
+            Bucket: bucketName,
+            Body: body,
+            ACL: 'public-read',
+            ContentType: lookup(file) || 'text/plain',
+        });
+    }));
+}
 
-// In terms of versioning DO NOT use branches, we JUST will use tags.
-// Initial tags will be `v1` and `v1.0.0`
-// - https://github.com/actions/toolkit/blob/main/docs/action-versioning.md
+run().catch(err => {
+    core.error(err);
+    core.setFailed(err.message);
+});
