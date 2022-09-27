906d5fdcd838e44fa46cabb9355aad47b5a0c97f
Quintin Willison <q@qwuk.net>
2022-09-27 11:27:45 +0100

    Patch Attempt 3: for when artifactName is empty string.
    The Error we are seeing when the action is run before this change:
    Validation Failed: {"resource":"Deployment","code":"custom","field":"task","message":"task is too short (minimum is 1 character)"}
    
    Fixing oversight from this change:
    https://github.com/ably/sdk-upload-action/pull/46

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ const createDeployment = async () => {
     const response = await octokit.repos.createDeployment({
         ...context.repo,
         ref: githubDeploymentRef,
-        task: artifactName || 'root',
+        task: (artifactName && artifactName.length > 0) ? artifactName : 'root',
         required_contexts: [],
         githubEnvironmentName,
         auto_merge: false,
